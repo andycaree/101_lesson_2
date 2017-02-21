88eccd9522ac4828b0c91a422af3df5b521b6b54
Andy <andycaree@gmail.com>
2017-02-21 00:10:07 +0000

    02-20 update

--- mortgage.rb
+++ mortgage.rb
@@ -1,13 +1,13 @@
-def negative_empty(num)
-  num.to_f < 0 || num.empty?
+def negative_or_empty?(num)
+  num.to_f < 0 || num.empty? || num.to_i == 0
 end
 
 loan_amount = ''
 loop do
   puts "What is the loan amount?"
   loan_amount = gets.chomp.gsub(/([$,])/, '') # removes the '$' and ','' symbols
-  if negative_empty(loan_amount)
-    puts "Please enter a number that is greater than zero."
+  if negative_or_empty?(loan_amount)
+    puts "Please enter a number greater than zero."
   else
     break
   end
@@ -17,19 +17,19 @@ apr = ''
 loop do
   puts "Please choose an Annual Percentage Rate (APR)."
   apr = gets.chomp.gsub(/([%])/, '') # removes the '%' symbol
-  if negative_empty(apr)
-    puts "Please enter a number that is zero or greater."
+  if negative_or_empty?(apr)
+    puts "Please enter a number greater than zero."
   else
     break
   end
 end
-mpr = (apr.to_f / 12) / 100
+monthly_rate = (apr.to_f / 12) / 100
 
 years = ''
 loop do
   puts "In years, what is the duration of the loan?"
   years = gets.chomp
-  if negative_empty(years)
+  if negative_or_empty?(years)
     puts "Please enter a number greater than zero."
   else
     break
@@ -37,6 +37,7 @@ loop do
 end
 loan_length = years.to_f * 12
 
-monthly_payment = loan_amount.to_f * (mpr.to_f / (1 - (1 + mpr.to_f)**-loan_length.to_f))
+monthly_payment = loan_amount.to_f *
+                  (monthly_rate / (1 - (1 + monthly_rate)**-loan_length))
 
 puts "Your total monthly payment is $#{format('%.2f', monthly_payment)}."
